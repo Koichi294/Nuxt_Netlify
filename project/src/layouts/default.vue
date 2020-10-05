f1a56bd02fed0fd5bbf09ddc0bb3520801d06a57
福島浩一郎 <fukushima@nlinks.co.jp>
2020-10-05 18:09:13 +0900

    todoリスト追加

--- project/src/layouts/default.vue
+++ project/src/layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -15,7 +27,7 @@
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
       <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
@@ -61,24 +73,29 @@ export default {
         {
           icon: "mdi-apps",
           title: "Welcome",
-          to: "/",
+          to: "/"
         },
         {
           icon: "mdi-chart-bubble",
           title: "Inspire",
-          to: "/inspire",
+          to: "/inspire"
         },
         {
           icon: "mdi-application",
-          title: "vuexTest",
-          to: "/vuexTest",
+          title: "VuexTest",
+          to: "/vuexTest"
         },
+        {
+          icon: "mdi-application",
+          title: "Todo",
+          to: "/todo"
+        }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: "Vuetify.js",
+      title: "Vuetify.js"
     };
-  },
+  }
 };
 </script>
