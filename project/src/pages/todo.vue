81f3e303aa1c8064a0f9edfb053b059750fd3ef3
福島浩一郎 <fukushima@nlinks.co.jp>
2020-10-06 17:38:27 +0900

    todo：チェックボックス追加

--- project/src/pages/todo.vue
+++ project/src/pages/todo.vue
@@ -5,21 +5,18 @@
         <v-text-field label="入力" v-model="content"></v-text-field>
       </v-col>
       <v-col cols="12" sm="6" md="3">
-        <v-btn @click="$store.commit('todo/add', content)" color="primary"
-          >追加</v-btn
-        >
+        <v-btn @click="add(content)" color="primary"> 追加 </v-btn>
       </v-col>
     </v-row>
 
     <v-simple-table height="300px">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th></th>
-        </tr>
-      </thead>
       <tbody>
         <tr v-for="item in list" :key="item.content">
+          <td style="width: 40px">
+            <v-checkbox
+              @click="$store.commit('todo/toggle', item)"
+            ></v-checkbox>
+          </td>
           <td>{{ item.content }}</td>
           <td style="width: 40px">
             <v-btn
@@ -27,8 +24,35 @@
               small
               @click="$store.commit('todo/delete', item.content)"
               color="error"
-              ><v-icon dark> mdi-minus </v-icon></v-btn
             >
+              <v-icon dark> mdi-minus </v-icon>
+            </v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+
+    <v-simple-table height="300px">
+      <tbody>
+        <tr v-for="item in doneList" :key="item.content">
+          <td style="width: 40px">
+            <v-checkbox
+              :input-value="item.isDone"
+              @click="$store.commit('todo/toggle', item)"
+            ></v-checkbox>
+          </td>
+          <td>
+            <font color="gray">{{ item.content }}</font>
+          </td>
+          <td style="width: 40px">
+            <v-btn
+              fab
+              small
+              @click="$store.commit('todo/delete', item.content)"
+              color="error"
+            >
+              <v-icon dark> mdi-minus </v-icon>
+            </v-btn>
           </td>
         </tr>
       </tbody>
@@ -37,6 +61,7 @@
 </template>
 
 <script>
+import { mapMutations } from "vuex";
 export default {
   data: () => {
     return {
@@ -45,7 +70,22 @@ export default {
   },
   computed: {
     list() {
-      return this.$store.getters["todo/list"];
+      const list = this.$store.getters["todo/list"].filter(
+        (todo) => todo.isDone == false
+      );
+      return list;
+    },
+    doneList() {
+      const list = this.$store.getters["todo/list"].filter(
+        (todo) => todo.isDone == true
+      );
+      return list;
+    },
+  },
+  methods: {
+    add(content) {
+      this.$store.commit("todo/add", content);
+      this.content = "";
     },
   },
 };
